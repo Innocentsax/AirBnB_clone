787526a7a6488e8990879f7069e0a1823b526f35
Ben Asmah <113874303+drbenasmah@users.noreply.github.com>
2023-05-10 21:08:11 +0200

    Create city.py
    city

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Contains the City model"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Implements the City class"""
+    state_id = ""
+    name = ""
