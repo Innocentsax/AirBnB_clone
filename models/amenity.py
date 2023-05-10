1d5a63e5d2e6e28498891ec92c66df7a38030a6a
Innocentsax <innocentcharlesudo@gmail.com>
2023-05-10 20:00:24 +0100

    AirBnB clone Team Project

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" module for Amenity class """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    initiation of Amenity that inherits from BaseModel class
+
+    Public Class Attributes:
+    (string) name: initialized as empty string
+    """
+    name = ""
