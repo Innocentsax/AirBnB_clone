697fe8e27d0869bffc6a1014509b535c87062dee
Ben Asmah <113874303+drbenasmah@users.noreply.github.com>
2023-05-10 21:12:13 +0200

    Create state.py
    state

---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A module containing the State model"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Implements the State model for any state object"""
+    name = ""
