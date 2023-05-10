5eedf2554ab4b24b0b02b721d22e534bc9ecd6dc
Ben Asmah <113874303+drbenasmah@users.noreply.github.com>
2023-05-10 21:09:46 +0200

    Create place.py
    place

---
+++ models/place.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Contains the Place model"""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """
+    Implements the Place model
+
+    Args:
+        city_id (str): The City id.
+        user_id (str): The User id.
+        name (str): The name of the place.
+        description (str): The description of the place.
+        number_rooms (int): The number of rooms of the place.
+        number_bathrooms (int): The number of bathrooms of the place.
+        max_guest (int): The maximum number of guests of the place.
+        price_by_night (int): The price by night of the place.
+        latitude (float): The latitude of the place.
+        longitude (float): The longitude of the place.
+        amenity_ids (list): A list of Amenity ids.
+    """
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
