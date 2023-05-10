10c2c60b01bc944034b14ec9be701ded203ebd6b
Ben Asmah <113874303+drbenasmah@users.noreply.github.com>
2023-05-10 21:18:13 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Contains the City model"""
+"""This contains the City model"""
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """Implements the City class"""
+    """This implements the City class"""
     state_id = ""
     name = ""
