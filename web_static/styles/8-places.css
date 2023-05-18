3a932e96bcac58217a13da8ab165ee8532b44d8f
innocentsax <innocentcharlesudo@gmail.com>
2023-05-18 07:57:12 +0100

    Web Static AirBnB

---
+++ web_static/styles/8-places.css
@@ -0,0 +1,78 @@
+.places h1{
+    text-align: left;
+    font-size: 30px;
+    width: 100%;
+}
+
+.places article{
+    width: 390px;
+    border: 1px solid #FF5A5F;
+    border-radius: 4px;
+    padding: 20px;
+    margin: 20px;
+    display: inline-block;
+    text-align: center;
+}
+
+.places {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+.price_by_night {
+    color: #FF5A5F;
+    border: 4px solid #FF5A5F;
+    border-radius: 50%;
+    min-width: 60px;
+    width: 0px;
+    height: 60px;
+    font-size: 30px;
+
+    position: relative;
+    bottom: 71px;
+    left: 322px;
+    line-height: 2;
+    padding: 8px;
+}
+
+.information {
+    height: 80px;
+    border-top: 1px solid #DDDDDD;
+    border-bottom: 1px solid #DDDDDD;
+    padding-top: 10px;
+    margin-top: -40px;
+
+}
+
+.max_guest {
+    background-image: url("../images/icon_group.png");
+    background-repeat: no-repeat;
+    width: 100px;
+}
+
+.number_rooms {
+    background-image: url("../images/icon_bed.png");
+    background-repeat: no-repeat;
+    width: 100px;
+}
+
+.number_bathrooms {
+    background-image: url("../images/icon_bath.png");
+    background-repeat: no-repeat;
+    width: 100px;
+
+}
+
+.max_guest, .number_rooms, .number_bathrooms {
+    display: inline-block;
+    height: 100px;
+    background-position: 24px 20px;
+}
+
+
+.user, .description {
+    text-align: left;
+    padding: 5px;
+}
